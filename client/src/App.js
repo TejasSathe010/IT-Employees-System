6dd967f9e387b356b25ef28bc6c4653448b53fbb
Tejas Sathe <tejassathe@Tejass-Air.lan>
2023-12-04 00:34:00 -0500

    Setup dashboard menu routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import Employee from './components/Employee';
+import Category from './components/Category';
+import Profile from './components/Profile';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
  
 function App() {
@@ -9,7 +13,12 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/adminlogin' element={<Login />} />
-          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/dashboard' element={<Dashboard />}>
+            <Route path='' element={<Home />} />
+            <Route path='/dashboard/employee' element={<Employee />} />
+            <Route path='/dashboard/category' element={<Category />} />
+            <Route path='/dashboard/profile' element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
